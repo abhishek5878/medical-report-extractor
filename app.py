4c287c306ab566c30b7e123762792c60086743ac
Abhishek Vyas <abhishekvyas02032001@gmail.com>
2025-05-04 20:02:34 +0530

    Move template to templates directory and update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def allowed_file(filename):
 
 @app.route('/')
 def index():
-    return render_template('html_template.html')
+    return render_template('index.html')
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
